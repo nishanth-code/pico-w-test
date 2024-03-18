bc85a3d8502a5a89a2858cef506e02dcd1351d84
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2024-03-18 11:04:39 +0000

    final

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app =  express()
+const cors = require('cors')
+app.use(cors())
+
+app.get('/pico',(req,res)=>{
+    console.log('hi')
+    res.json({message:"hello from node server"})
+})
+app.post('/pico',(req,res)=>{
+    console.log(req.body);
+})
+
+app.listen(3000,()=>{
+    console.log('server running');
+})
